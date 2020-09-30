c0769bdb2a1147e6c96627452742ee32e65dc8aa
Durtanyan <you@example.com>
2020-09-30 22:06:49 +0300

    FizzBuzz

---
+++ fizz_buzz.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 21:30:31 2020
+
+@author: lukin
+"""
+"""
+Задача #2: FizzBuzz
+Если число делится на 3, то нужно вывести Fizz,
+если на 5, то Buzz, если на 3 и на 5, то FizzBuzz.
+
+Задача - написать программу, 
+которая выводит сумму чисел из диапазона от 1000 до 20000 включительно, 
+которые делятся и на 3 и на 5.
+"""
+summ = 0
+integer = 1000
+print(integer)
+while integer <= 20000:
+    if integer % 3 == 0:
+        print('Fizz')
+        if integer % 5 == 0:
+            print('Buzz')
+        if (integer % 3 == 0 and integer % 5 == 0):
+            print('FizzBuzz')
+            summ += integer
+    integer += 1
+print(f'Сумма чисел из диапазона от 1000 до 20000 включительно равна: summ')
